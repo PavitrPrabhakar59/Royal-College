bfef2227c168ae1adec5e6464d913c6b454bab06
AjayPandey59 <168255592+AjayPandey59@users.noreply.github.com>
2024-05-24 10:51:24 +0530

    Added Responsiveness for About Webpage and Some Fixes

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-window.onscroll = function () { myFunction() };
+var scrollElement = document.querySelector('#scroll');
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
-
+window.addEventListener('scroll', function () {
+  var height = document.body.scrollHeight - this.innerHeight;
+  var scrolledPixels = this.scrollY;
+  var width = ((scrolledPixels / height) * 100).toFixed(2);
+  scrollElement.style.width = width + '%';
+});
 var navLinks = document.getElementById("navLinks");
 function showMenu() {
   navLinks.style.right = "0";
