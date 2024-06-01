78994ccba93491cd14d1a7e75964269d4fa0b620
AjayPandey59 <168255592+AjayPandey59@users.noreply.github.com>
2024-06-01 21:53:25 +0530

    Minor UX Fixes

--- script.js
+++ script.js
@@ -17,15 +17,52 @@ function hideMenu() {
 window.addEventListener('load', function () {
   var imgan = document.querySelectorAll("div.imgan");
 
-  imgan.forEach(function (element) {
+  imgan.forEach(function (element, index) {
     var img = element.querySelector("img");
     if (img.complete) {
       checkImageDimensions(img, element);
+      img.addEventListener("click", () => {
+        openPopup(index);
+      })
     }
   });
   function checkImageDimensions(img, element) {
     if (img.naturalWidth < img.naturalHeight) {
       element.classList.add("imgan-h");
     }
+  }
+});
+
+let currentIndex;
+
+function openPopup(index) {
+  currentIndex = index;
+  const popup = document.getElementById("popup");
+  const popupImg = document.getElementById("popup-img");
+  popupImg.src = document.getElementsByClassName("imgan")[index].getElementsByTagName("img")[0].src;
+  popup.style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + document.getElementsByClassName("imgan").length) % document.getElementsByClassName("imgan").length;
+  document.getElementById("popup-img").src = document.getElementsByClassName("imgan")[currentIndex].getElementsByTagName("img")[0].src;
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % document.getElementsByClassName("imgan").length;
+  document.getElementById("popup-img").src = document.getElementsByClassName("imgan")[currentIndex].getElementsByTagName("img")[0].src;
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    prevImage();
+  } else if (event.key === "ArrowRight") {
+    nextImage();
+  } else if (event.key === "Escape") {
+    closePopup();
   }
 });
